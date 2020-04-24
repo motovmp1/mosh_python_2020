55182fc22bf2dca554bb1e8b25daeac3fa2a0a75
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-24 15:52:30 +0800

    Running code 4

--- timestamp.py
+++ timestamp.py
@@ -1,5 +1,17 @@
 import time
 
 
-print(time.time())
+def send_emails():
+    for i in range(200):
+        time.sleep(0.02)
+        print(i)
+        pass
+
+
+start = time.time()
+send_emails()
+end = time.time()
+duration = end - start
+# this is the way that we can print 2 decimal point python / there are many
+print("%.2f" % duration)
 
