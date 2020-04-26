66d9973bbb29ba5fe6ed0c5dddaef8773cc6190c
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-26 14:58:23 +0800

    Running code 11

--- command_line_arguments.py
+++ command_line_arguments.py
@@ -13,7 +13,14 @@ print("return_code", completed.returncode)
 print("std_err", completed.stderr)
 print("std_out", completed.stdout.decode())
 
+
+# always need to check is the system fail or not
 completed2 = subprocess.run(["python3", "/home/elsys/PycharmProjects/mosh_python_2020/python_run_terminal.py"], stdout=PIPE)
 time.sleep(1)
 print("std_out", completed2.stdout.decode())
+print("return_code", completed2.returncode)
+if completed2.returncode != 0:
+    print(completed2.stderr)
+else:
+    print("PASS")
 
