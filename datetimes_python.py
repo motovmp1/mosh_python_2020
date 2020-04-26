8c1c0191e96a55ffa6ac6bdc6cf2098ec42df20d
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-26 13:39:47 +0800

    Running code 7

--- datetimes_python.py
+++ datetimes_python.py
@@ -1,8 +1,24 @@
+import time
 from datetime import datetime
 
 dt = datetime(2018, 1, 1)
 
 print(datetime.now())
 
-datetime.strptime("2018/01/01", "%Y/%m/%d")
+dt1 = datetime.strptime("2018/01/01", "%Y/%m/%d")
 
+# this is a datetime format print
+print(type(dt1))
+
+dt2 = datetime.fromtimestamp(time.time())
+
+print(dt2)
+
+
+print(f'{dt2.year}/{dt2.month}')
+
+
+# this is a string format date
+print(dt.strftime("%Y/%m"))
+
+print(type(dt.strftime("%Y/%m")))
